18e6c54f595b9bcc221444c381f993e81f4c7946
elhane <elena.gracheva.639@gmail.com>
2021-04-18 22:54:40 +0500

    правит отступы

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <main id="app">
+  <main class="main" id="app">
     <router-view/>
   </main>
 </template>
 
 <style lang="scss">
-
+.main {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
 </style>
